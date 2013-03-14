4c6a5b13e3db60e52fd7749e0a75e91205f518d1
Victor Coulon <victor@hull.io>
2013-03-14 16:13:02 +0100

    Update Identity doc

--- widgets/identity/main.js
+++ widgets/identity/main.js
@@ -1,26 +1,19 @@
 /**
- * # Widget `identity`
  *
- * This widget is identified as `identity@hull`  
+ * This widget is identified as `identity@hull`
  * Use it to indicate whether the user is logged in or not, and display login buttons if not.
  *
+ * ## Templates
+ *
+ * - `identity`: Displays login buttons or the name of the user, depending on its logged in state
+ * 
  */
-
-
 
 define({
   type: "Hull",
   
-  /**
-   * @name Templates
-   * #### `identity/identity`
-   * 
-   * Displays login buttons or the name of the user, depending on its logged in state
-   *
-   */
   templates: ['identity'],
 
-  // Which events to refresh the widget on.
   refreshEvents: ['model.hull.me.change'],
 
   initialize: function() {
@@ -30,13 +23,6 @@ define({
     }
   },
 
-  /**
-   * Process the resolved datasources before sending them to the template
-   *
-   * @param {Object} data The resolved requests from the `datasources` object
-   * @return {Object|Promise|undefined}     The data after you processed it.
-   * @name beforeRender
-   */
   beforeRender: function(data) {
     data.authServices = this.authServices || [];
     return data;
